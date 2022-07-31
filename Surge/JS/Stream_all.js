dbee57a4821f7eefa3aeca17c63912cee3e330ec
Scpshaku <84615903+Scpshaku@users.noreply.github.com>
2022-07-31 12:38:41 +0800

    Update Stream_all.js

--- Surge/JS/Stream_all.js
+++ Surge/JS/Stream_all.js
@@ -151,7 +151,7 @@ panel_result['content'] = content
         if (code === 'Not Found') {
           return inner_check(80018499)
         }
-        netflix_check_result += '完整解，区域: ' + code.toUpperCase()
+        netflix_check_result += '已解锁，区域: ' + code.toUpperCase()
         return Promise.reject('BreakSignal')
       })
       .then((code) => {
