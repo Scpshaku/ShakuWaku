4ea11c1c8c3e879aaaa98dc179e47873bf13ac11
Scpshaku <84615903+Scpshaku@users.noreply.github.com>
2022-07-31 09:22:30 +0800

    Update Stream_all.js

--- Surge/JS/Stream_all.js
+++ Surge/JS/Stream_all.js
@@ -1,11 +1,3 @@
-/*
- * 由@LucaLin233编写
- * 原脚本地址：https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/stream-all.js
- * 由@ShakuWaku修改
- * 更新日期：2022.07.22
- * 版本：2.3
- */
-
 const REQUEST_HEADERS = {
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
@@ -227,7 +219,7 @@ panel_result['content'] = content
       function getLocationInfo() {
         return new Promise((resolve, reject) => {
           let opts = {
-            url: 'https://disney.api.edge.bamgrid.com/v1/public/graphql',
+            url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
             headers: {
               'Accept-Language': 'en',
               Authorization: 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
